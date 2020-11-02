f3408ec1b9bb227b4b1328804935c8229be7485b
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2020-11-02 17:48:39 -0500

    joelle weather app attempt

--- src/pages/CitySearch.js
+++ src/pages/CitySearch.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React { useState, useEffect } from 'react';
+import axios from 'axios'
+import { Link } from 'react-router-dom'; 
 
 const CitySearch = () => {
+
+  const [cities, setCities] = useState([]);
+
+  const handleSearch = (e) => {
+    fetch(`https://wyn-weather-api.herokuapp.com/cities?query=${e.target.value}`)
+
+    .then((response) => response.json())
+    .then((cities) => setCities(cities));
+    console.log(cities);
+  };
+
+
     return(
+      <div>
       <h1>City Search</h1>
-    )
-  }
+      <input type="text" onChange={handleSearch}/>
+      <ul>
+       {cities.map((city) => {
+         return 
+         <li key={city.id}>
+           <Link to={`/cities/${city.id}`}>{city.name}</Link>
+           </li>;
+         })}
+         </div>
+    );
+  };
 
 export default CitySearch
--- src/pages/SingleCity.js
+++ src/pages/SingleCity.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import moment from 'moment';
 
 const SingleCity = () => {
+  const [city, setCity] = useState({});
+  const {id} = useParams();
+
+  useEffect(() => {
+
+        fetch(`https://wyn-weather-api.herokuapp.com/cities/${id}`);
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setCity(data);
+        });
+      });
+  
   return(
-    <h1>Single City</h1>
-  )
+    <div>
+    <h1>{city.name}</h1>
+    <div className="forecasts">
+      {city.forecasts && city.forecasts.map((forecast) => {
+        return (
+          <>
+          <h3>{moment{forecast.date}.format('MMM D')}</h3>
+          <img src={forecast.image} alt={forecast.name} />);
+          <h3>{forecast.weather}</h3>
+          <p>{forecast.min_temp.toFixed()}</p>
+          <p>{forecast.max_temp.toFixed()}</p>
+          <p>{forecast.humidity}</p>
+      })}
+    </div>
+    </div>
+  );
 }
 
 export default SingleCity
